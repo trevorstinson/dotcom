397862740fe73aaf474dbbc728f4f21161601f51
Trevor Stinson <trevor@trevorstinson.com>
2023-06-07 00:01:10 -0500

    Minimal metadata and layout customizations

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Trevor Stinson",
+	url: "https://trevorstinson.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Trevor Stinson's personal website.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Trevor Stinson",
+		email: "rarest-dobro0y@icloud.com",
+		url: "https://example.com/about/"
 	}
 }
