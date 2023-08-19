e770369c983f3a1e0133e4821b98e4b2a4219b8f
Trevor Stinson <trevor@trevorstinson.com>
2023-08-19 15:19:08 -0500

    Move robots.txt  to /public. Closes #8

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,8 +15,7 @@ module.exports = function(eleventyConfig) {
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
 		"./public/": "/",
-		"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css",
-		"src/robots.txt": "/robots.txt"
+		"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css"
 	});
 
 	eleventyConfig.addPassthroughCopy({
