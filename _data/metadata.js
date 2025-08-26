2032b5bf0fbcd5411dc5abd97e5125b3a13da2ef
Trevor Stinson <trevor@trevorstinson.com>
2025-08-26 16:55:26 -0500

    Personalize metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Trevor Stinson",
+	url: "https://trevorstinson.com",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Trevor Stinson's personal website",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
-}
+		name: "Trevor Stinson",
+		email: "rarest-dobro0y@icloud.com",
+		url: "https://trevorstinson.com/about",
+	},
+};
